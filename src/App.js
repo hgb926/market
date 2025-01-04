68dc6df2c2e58ecd2e0181eae3c877ecd001d3d5
hgb926 <hgb926@naver.com>
2025-01-04 18:10:04 +0900

    [Feat] route 활성화
    RootLayout 렌더링

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
+import {RouterProvider} from "react-router-dom";
+import {router} from "./config/route-config";
 
 function App() {
     return (
-        <div>
-            <h1>Hello, React 18!</h1>
-        </div>
+        <RouterProvider router={router}/>
     );
 }
 

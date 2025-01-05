4070c77c7b51537533eb86e37dcc2d90f559f91d
hgb926 <hgb926@naver.com>
2025-01-05 13:59:11 +0900

    [Init] 메인 레이아웃들 생성

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -1,12 +1,24 @@
 import {createBrowserRouter} from "react-router-dom";
 import RootLayout from "../layout/RootLayout";
 import ErrorPage from "../pages/ErrorPage";
+import MainPage from "../pages/MainPage";
 
+
+const homeRouter = [
+
+]
 
 export const router = createBrowserRouter([
     {
         path: "/",
         element: <RootLayout />,
         errorElement: <ErrorPage/>,
+        children: [
+            {
+                path: '/',
+                element: <MainPage/>,
+                children: homeRouter
+            }
+        ]
     }
 ])

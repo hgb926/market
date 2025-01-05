89b235c1005236e2548a11cbc86ba266260b97bc
hgb926 <hgb926@naver.com>
2025-01-05 15:37:29 +0900

    [Feat] 페이지 라우팅 설정

---
+++ src/components/HomeNavigation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '../styles/components/HomeNavigation.module.scss'
+
+const HomeNavigation = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default HomeNavigation;
--- src/layout/MainSection.js
+++ src/layout/MainSection.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import styles from '../styles/layout/MainSection.module.scss'
+import {Outlet} from "react-router-dom";
 
 const MainSection = () => {
     return (
         <div className={styles.container}>
-            main section
+            <Outlet/>
         </div>
     );
 };

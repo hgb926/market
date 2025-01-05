0863ddba217fa144c0d229be3d536dbacb714b06
hgb926 <hgb926@naver.com>
2025-01-05 16:25:27 +0900

    [Design] 더미데이터 사진 추가

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import HomeNavigation from "../components/HomeNavigation";
 import styles from '../styles/pages/Home.module.scss'
+import Posts from "./Posts";
 
 const Home = () => {
     return (
         <div className={styles.container}>
             <HomeNavigation/>
+            <Posts/>
         </div>
     );
 };

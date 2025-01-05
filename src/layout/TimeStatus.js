e967395d030115293d91d943591d1eec4362266c
hgb926 <hgb926@naver.com>
2025-01-05 14:29:50 +0900

    [Design] 배경색상 #2b2a2a

--- src/layout/TimeStatus.js
+++ src/layout/TimeStatus.js
@@ -2,9 +2,19 @@ import React from 'react';
 import styles from '../styles/layout/TimeStatus.module.scss'
 
 const TimeStatus = () => {
+
+    let time = new Date().getHours() + ":" + new Date().getMinutes();
+
+
     return (
         <div className={styles.container}>
-            time
+            <div className={styles.time}>{time}</div>
+            <div className={styles.notchWrap}>
+                <div className={styles.notch}></div>
+            </div>
+            <div className={styles.others}>
+
+            </div>
         </div>
     );
 };

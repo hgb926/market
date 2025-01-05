9284e8b794317f911dd78e7862ca5123d27530da
hgb926 <hgb926@naver.com>
2025-01-05 14:47:17 +0900

    [Design] TimeStatus 기본 레이아웃

--- src/layout/TimeStatus.js
+++ src/layout/TimeStatus.js
@@ -1,19 +1,44 @@
-import React from 'react';
-import styles from '../styles/layout/TimeStatus.module.scss'
+import React, {useEffect, useState} from 'react';
+import styles from '../styles/layout/TimeStatus.module.scss';
+import {BsBarChartFill} from 'react-icons/bs';
+import {TiWiFi} from 'react-icons/ti';
+import {CgBatteryFull} from 'react-icons/cg';
 
 const TimeStatus = () => {
+    const [time, setTime] = useState('');
 
-    let time = new Date().getHours() + ":" + new Date().getMinutes();
+    // 시간 업데이트
+    useEffect(() => {
+        const updateTime = () => {
+            const now = new Date();
+            const hours = now.getHours().toString().padStart(2, '0');
+            const minutes = now.getMinutes().toString().padStart(2, '0');
+            setTime(`${hours}:${minutes}`);
+        };
 
+        updateTime();
+        const timer = setInterval(updateTime, 1000); // 매 초마다 업데이트
+
+        return () => clearInterval(timer);
+    }, []);
 
     return (
         <div className={styles.container}>
+            {/* 시간 */}
             <div className={styles.time}>{time}</div>
+
+            {/* 노치 */}
             <div className={styles.notchWrap}>
                 <div className={styles.notch}></div>
             </div>
-            <div className={styles.others}>
 
+            {/* 기타 아이콘 */}
+            <div className={styles.others}>
+                <div className={styles.iconWrap}>
+                    <BsBarChartFill className={styles.signal}/>
+                    <TiWiFi className={styles.wifi}/>
+                    <CgBatteryFull className={styles.battery}/>
+                </div>
             </div>
         </div>
     );

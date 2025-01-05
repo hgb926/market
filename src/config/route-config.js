3d994a3362a4d1939204d2b7ca847dfa05769e0a
hgb926 <hgb926@naver.com>
2025-01-05 15:37:29 +0900

    [Feat] 페이지 라우팅 설정

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -2,10 +2,29 @@ import {createBrowserRouter} from "react-router-dom";
 import RootLayout from "../layout/RootLayout";
 import ErrorPage from "../pages/ErrorPage";
 import MainPage from "../pages/MainPage";
+import Home from "../pages/Home";
+import Chat from "../pages/Chat";
+import MyPage from "../pages/MyPage";
+import MapPage from "../pages/Map";
 
 
 const homeRouter = [
-
+    {
+        index: true,
+        element: <Home/>,
+    },
+    {
+        path: 'chat',
+        element: <Chat/>,
+    },
+    {
+        path: 'map',
+        element: <MapPage/>,
+    },
+    {
+        path: 'info',
+        element: <MyPage/>
+    }
 ]
 
 export const router = createBrowserRouter([
---
+++ src/pages/MyPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyPage = () => {
+    return (
+        <div>
+            my page
+        </div>
+    );
+};
+
+export default MyPage;

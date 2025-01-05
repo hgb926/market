be7aaf5dc12aa7c4d92d7afbbf8467b3783ec3ef
hgb926 <hgb926@naver.com>
2025-01-05 15:55:33 +0900

    [Design] HomeNavigation 디자인 완료

--- src/components/HomeNavigation.js
+++ src/components/HomeNavigation.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import styles from '../styles/components/HomeNavigation.module.scss'
+import {IoIosArrowDown, IoMdSearch} from "react-icons/io";
+import {RxHamburgerMenu} from "react-icons/rx";
+import {LuBell} from "react-icons/lu";
 
 const HomeNavigation = () => {
     return (
-        <div>
-
+        <div className={styles.container}>
+            <div className={styles.town}>백석동 <IoIosArrowDown className={styles.arrow}/></div>
+            <div className={styles.empty}></div>
+            <div className={styles.menus}>
+                <RxHamburgerMenu />
+                <IoMdSearch />
+                <LuBell />
+            </div>
         </div>
     );
 };

103401670f4ab030a8bec2cf9fab385b3bc0e24b
hgb926 <hgb926@naver.com>
2025-01-05 13:59:33 +0900

    [Init] 라우팅 생성

---
+++ src/layout/TimeStatus.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '../styles/layout/TopStatus.module.scss'
+
+const TimeStatus = () => {
+    return (
+        <div>
+            time
+        </div>
+    );
+};
+
+export default TimeStatus;

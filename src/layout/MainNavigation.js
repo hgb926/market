c44147dff1b68677b0a4bb51abe0baf658af25c4
hgb926 <hgb926@naver.com>
2025-01-05 15:03:57 +0900

    [Design] MainNavigation 항목 생성
    크기 조절 x

--- src/layout/MainNavigation.js
+++ src/layout/MainNavigation.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import styles from '../styles/layout/MainNavigation.module.scss'
+import {HiMiniHome} from "react-icons/hi2";
+import {RiMapPin2Fill} from "react-icons/ri";
+import {BsChatDotsFill} from "react-icons/bs";
+import {FaUser} from "react-icons/fa";
 
 const MainNavigation = () => {
     return (
         <div className={styles.container}>
-            mainNavi
+            <div className={styles.navWrap}>
+                <div className={styles.menu}>
+                    <HiMiniHome />
+                    <p>홈</p>
+                </div>
+                <div className={styles.menu}>
+                    <BsChatDotsFill />
+                    <p>채팅</p>
+                </div>
+                <div className={styles.menu}>
+                    <RiMapPin2Fill />
+                    <p>동네지도</p>
+                </div>
+                <div className={styles.menu}>
+                    <FaUser />
+                    <p>나의 정보</p>
+                </div>
+            </div>
         </div>
     );
 };

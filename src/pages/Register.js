65a3312b5b07be0895bdc1e13ff1afaaceccba12
hgb926 <hgb926@naver.com>
2025-01-07 23:12:45 +0900

    [Design] 회원가입 스텝바 생성

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from '../styles/pages/Register.module.scss'
 
 const Register = () => {
+    // email, password, username, address, profileImage 받아야함
+    // Register.module.scss에서 다 처리하도록?
+    // step도
+    const [currentStep, setCurrentStep] = useState(1)
 
-    console.log('register')
+    const increase = () => {
+        setCurrentStep(prev => prev +1)
+    }
     return (
-        <div>
-            register
+        <div className={styles.container}>
+            <div className={styles.stepBar}>
+                <div className={styles.dynamic} style={{
+                    width: `${currentStep * 20}%`
+                }}></div>
+            </div>
+            <div onClick={increase}>z</div>
         </div>
     );
 };

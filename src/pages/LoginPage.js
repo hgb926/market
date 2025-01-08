081f4fb3c66b78f6f6ade7ab9c10318367fb2850
hgb926 <hgb926@naver.com>
2025-01-08 21:48:43 +0900

    [UI] 자동로그인 ui 추가

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,18 +1,63 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import styles from '../styles/pages/LoginPage.module.scss'
 import {Link} from "react-router-dom";
+import {validateEmail, validatePassword,} from '../utils/validation';
 
 const LoginPage = () => {
+
+    const [email, setEmail] = useState('')
+    const [pw, setPw] = useState('')
+    const [active, setActive] = useState(false)
+    const emailRef = useRef();
+    const pwRef = useRef();
+
+    const loginHandler = async () => {
+        if (!active) return
+        const payload = {
+            email,
+            password: pw
+        }
+    }
+
+    useEffect(() => {
+        if (validateEmail(email) && validatePassword(pw)) setActive(true)
+        else setActive(false)
+    }, [email, pw]);
+
     return (
         <>
             <span className={styles.topLine}/>
             <div className={styles.container}>
                 <h1 className={styles.title}>Market</h1>
                 <div className={styles.inputGroup}>
-                    <input type={"email"}  placeholder={'이메일'}/>
-                    <input type={'password'} placeholder={"비밀번호"}/>
+                    <input
+                        type={"email"}
+                        placeholder={'이메일'}
+                        ref={emailRef}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <input
+                        type={'password'}
+                        placeholder={"비밀번호"}
+                        ref={pwRef}
+                        onChange={(e) => setPw(e.target.value)}
+                    />
+                    <div className={styles.autoLoginWrap}>
+                        <input
+                            type={'checkbox'}
+                            id="autoLogin"
+                            className={styles.autoLogin}
+                        />
+                        <label htmlFor="autoLogin" className={styles.autoLoginLabel}>
+                            자동 로그인
+                        </label>
+                    </div>
                 </div>
-                <div className={styles.loginBtn}>로그인</div>
+                <div
+                    className={`${styles.loginBtn} ${active ? styles.active : ''}`}
+                    onClick={loginHandler}>
+                    로그인
+                </div>
                 <div className={styles.bottom}>
                     <Link to={'register'} className={styles.register}>회원가입</Link>
                     <div className={styles.findPw}>비밀번호 찾기</div>

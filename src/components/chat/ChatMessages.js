3e37d5ffeef0ad2f3d86a8d482276e1f89ae17a4
hgb926 <hgb926@naver.com>
2025-01-12 22:52:54 +0900

    [Feat] 채팅 날짜, 시간 렌더링

--- src/components/chat/ChatMessages.js
+++ src/components/chat/ChatMessages.js
@@ -1,7 +1,7 @@
-import React, { useEffect, useRef } from 'react';
+import React, {useEffect, useRef} from 'react';
 import styles from '../../styles/pages/ChatRoom.module.scss';
 
-const ChatMessages = ({ messages, userId, chat }) => {
+const ChatMessages = ({messages, userId, chat}) => {
     const chatContainerRef = useRef(null);
 
     // 메시지가 변경될 때 스크롤을 맨 아래로 이동
@@ -14,29 +14,41 @@ const ChatMessages = ({ messages, userId, chat }) => {
     return (
         <div className={styles.chatContainer} ref={chatContainerRef}>
             <div className={styles.systemNotice}>{chat.customerInfo.customerNickname}님께서 대화를 신청했습니다.</div>
-            {messages.map((msg) =>
-                msg.writer !== userId ? (
-                    <div className={styles.sellerChat} key={msg.date}>
-                        <div
-                            className={styles.sellerImage}
-                            style={{
-                                backgroundImage: `url(${userId === chat.customerInfo.customerId
-                                    ? chat.sellerInfo.sellerImage
-                                    : chat.customerInfo.customerImage})`,
-                                backgroundSize: 'cover',
-                                backgroundPosition: 'center',
-                            }}
-                        />
-                        <div className={styles.sellerText}>{msg.text}</div>
-                        <div className={styles.sellerSendTime}>{msg.formatTime[1]}</div>
-                    </div>
+            {messages.map((msg, idx) => {
+                const isDifferentDate = idx === 0 || msg.formatTime[0] !== messages[idx - 1]?.formatTime[0];
+                const isDifferentTime = idx === 0 || msg.formatTime[1] !== messages[idx - 1]?.formatTime[1];
+
+                return msg.writer !== userId ? (
+                    <>
+                        {isDifferentDate && <div className={styles.systemTime}>{msg.formatTime[0]}</div>}
+                        <div className={styles.sellerChat} key={msg.date}>
+                            <div
+                                className={styles.sellerImage}
+                                style={{
+                                    backgroundImage: `url(${userId === chat.customerInfo.customerId
+                                        ? chat.sellerInfo.sellerImage
+                                        : chat.customerInfo.customerImage})`,
+                                    backgroundSize: 'cover',
+                                    backgroundPosition: 'center',
+                                }}
+                            />
+                            <div className={styles.sellerText}>{msg.text}</div>
+                            <div className={styles.sellerSendTime}>{msg.formatTime[1]}</div>
+                        </div>
+                    </>
                 ) : (
-                    <div className={styles.myChat} key={msg.date}>
-                        <div className={styles.myTime}>{msg.formatTime[1]}</div>
-                        <div className={styles.myText}>{msg.text}</div>
-                    </div>
-                )
-            )}
+                    <>
+                        {isDifferentDate && <div className={styles.systemTime}>{msg.formatTime[0]}</div>}
+                        <div className={styles.myChat} key={msg.date}>
+
+                            <div className={styles.myTime}>
+                                {isDifferentTime ? msg.formatTime[1] : ""}
+                            </div>
+                            <div className={styles.myText}>{msg.text}</div>
+                        </div>
+                    </>
+                );
+            })}
         </div>
     );
 };

c1f7de258b0a63399cca6d38091f9b4312842a3f
hgb926 <hgb926@naver.com>
2025-01-12 21:31:25 +0900

    [Feat] 채팅방 이미지 반대로 바꾸고 시스템 알림

--- src/components/chat/ChatMessages.js
+++ src/components/chat/ChatMessages.js
@@ -13,14 +13,14 @@ const ChatMessages = ({ messages, userId, chat }) => {
 
     return (
         <div className={styles.chatContainer} ref={chatContainerRef}>
-            <div>{chat.customerInfo.customerNickname}님께서 대화를 신청했습니다.</div>
+            <div className={styles.systemNotice}>{chat.customerInfo.customerNickname}님께서 대화를 신청했습니다.</div>
             {messages.map((msg) =>
                 msg.writer !== userId ? (
                     <div className={styles.sellerChat} key={msg.date}>
                         <div
                             className={styles.sellerImage}
                             style={{
-                                backgroundImage: `url(${userId !== chat.customerInfo.customerId
+                                backgroundImage: `url(${userId === chat.customerInfo.customerId
                                     ? chat.sellerInfo.sellerImage
                                     : chat.customerInfo.customerImage})`,
                                 backgroundSize: 'cover',

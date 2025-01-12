949914c52bb0425981ddd6f4426cbb4c04e27dbd
hgb926 <hgb926@naver.com>
2025-01-12 22:41:15 +0900

    [Feat] 채팅 엔터키 활성화

--- src/components/chat/ChatInput.js
+++ src/components/chat/ChatInput.js
@@ -4,22 +4,32 @@ import { LuSendHorizontal } from 'react-icons/lu';
 import { RiEmotionHappyLine } from 'react-icons/ri';
 import styles from '../../styles/pages/ChatRoom.module.scss';
 
-const ChatInput = ({ text, onChange, onSend, inputRef }) => (
-    <div className={styles.inputContainer}>
-        <GoPlus className={styles.plus} />
-        <div className={styles.inputWrap}>
-            <input
-                type="text"
-                value={text}
-                onChange={(e) => onChange(e.target.value)}
-                className={styles.input}
-                ref={inputRef}
-                placeholder="메시지 보내기"
-            />
-            <RiEmotionHappyLine className={styles.emotion} />
+const ChatInput = ({ text, onChange, onSend, inputRef }) => {
+    // Enter 키 핸들러 함수
+    const enterHandler = (e) => {
+        if (e.key === 'Enter' && text.trim()) {
+            onSend();
+        }
+    };
+
+    return (
+        <div className={styles.inputContainer}>
+            <GoPlus className={styles.plus} />
+            <div className={styles.inputWrap}>
+                <input
+                    type="text"
+                    value={text}
+                    onChange={(e) => onChange(e.target.value)}
+                    onKeyDown={enterHandler} // Enter 핸들러 연결
+                    className={styles.input}
+                    ref={inputRef}
+                    placeholder="메시지 보내기"
+                />
+                <RiEmotionHappyLine className={styles.emotion} />
+            </div>
+            <LuSendHorizontal className={styles.send} onClick={onSend} />
         </div>
-        <LuSendHorizontal className={styles.send} onClick={onSend} />
-    </div>
-);
+    );
+};
 
 export default ChatInput;

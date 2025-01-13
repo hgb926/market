7d7a2da80581558a385a4125aa263189a2935bc4
hgb926 <hgb926@naver.com>
2025-01-13 19:29:18 +0900

    [Feat] 알림 애니메이션 처리

--- src/components/chat/ChatAlarm.js
+++ src/components/chat/ChatAlarm.js
@@ -1,35 +1,32 @@
-import React, {useEffect} from 'react';
-import styles from '../../styles/components/ChatAlarm.module.scss'
-import {useSelector} from "react-redux";
-import {Link} from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import styles from '../../styles/components/ChatAlarm.module.scss';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
-const ChatAlarm = ({loginFlag, message, setShowAlarm}) => {
-    let userData = useSelector(state => state.userInfo.userData);
-
-    const hideAlarm = () => setShowAlarm(false)
+const ChatAlarm = ({ loginFlag, message, setShowAlarm }) => {
+    const userData = useSelector((state) => state.userInfo.userData);
+    const [isFadingOut, setIsFadingOut] = useState(false);
 
     useEffect(() => {
-        console.log(userData); // userData 확인
-        setTimeout(() => {
-            hideAlarm()
-        }, 4000)
-    }, [userData]);
+        // 5초 후 사라지기 시작
+        const timeout = setTimeout(() => {
+            setIsFadingOut(true);
+        }, 5000);
 
-    // {
-    //     "_id": "6784e883f13c1364773f9249",
-    //     "room": "6784b546d0956b72c15bb711",
-    //     "text": "gd",
-    //     "writer": "6780c132f6b6ce277c05b8d1",
-    //     "date": "2025-01-13T10:18:43.650Z",
-    //     "taker": "6780f844841d0ca5d67b95b2",
-    //     "nickname": "스윙스",
-    //     "postTitle": "바이레도 로즈오브노맨즈 팝니다",
-    //     "profileUrl": "https://node-forum1217.s3.ap-northeast-2.amazonaws.com/profile-images/1736491314126_áá¡áá®á«áá©áá³.jpeg"
-    // }
+        // fadeOut 애니메이션 종료 후 컴포넌트 숨기기
+        const fadeOutTimeout = setTimeout(() => {
+            setShowAlarm(false);
+        }, 5500); // fadeOut 애니메이션 시간(0.5초) 포함
+
+        return () => {
+            clearTimeout(timeout);
+            clearTimeout(fadeOutTimeout);
+        };
+    }, [setShowAlarm]);
 
     return (
         <Link
-            className={styles.container}
+            className={`${styles.container} ${isFadingOut ? styles.fadeOut : ''}`}
             to={`/chat/${message.room}`}
         >
             <div className={styles.msgWrap}>
@@ -46,8 +43,8 @@ const ChatAlarm = ({loginFlag, message, setShowAlarm}) => {
                     <p className={styles.name}>{message.nickname}</p>
                     <p className={styles.text}>{message.text}</p>
                 </div>
-            {/*  제목 7글자 이상이면 ... 처리  */}
-            <div className={styles.postTitle}>{message.postTitle}</div>
+                {/* 제목 7글자 이상이면 ... 처리 */}
+                <div className={styles.postTitle}>{message.postTitle}</div>
             </div>
         </Link>
     );

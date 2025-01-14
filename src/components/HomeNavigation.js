8ce2770820faa95d5e8fffafc517a5d149e44cd8
hgb926 <hgb926@naver.com>
2025-01-14 19:18:44 +0900

    [Design] posts 스켈레톤 여백 조정

--- src/components/HomeNavigation.js
+++ src/components/HomeNavigation.js
@@ -6,19 +6,24 @@ import { LuBell } from 'react-icons/lu';
 import { IoSettingsOutline } from 'react-icons/io5';
 import HomeNavigationSkeleton from '../skeleton/HomeNavigationSkeleton';
 import NoticeModal from "./NoticeModal";
+import { useLocation } from "react-router-dom";
 
 const HomeNavigation = ({ mainText, arrow, bell, hamburger, search, setting }) => {
     const [loading, setLoading] = useState(true);
-    const [openNotice, setOpenNotice] = useState(false)
+    const [openNotice, setOpenNotice] = useState(false);
+    const { pathname } = useLocation();
 
     useEffect(() => {
-        // 500ms 후 로딩 완료
-        const timer = setTimeout(() => {
-            setLoading(false);
-        }, 500);
+        if (pathname === '/') {
+            const timer = setTimeout(() => {
+                setLoading(false);
+            }, 500);
 
-        return () => clearTimeout(timer);
-    }, []);
+            return () => clearTimeout(timer);
+        } else {
+            setLoading(false); // 다른 경로에서는 로딩 상태를 바로 false로 설정
+        }
+    }, [pathname]);
 
     if (loading) {
         return <HomeNavigationSkeleton />;
@@ -29,16 +34,16 @@ const HomeNavigation = ({ mainText, arrow, bell, hamburger, search, setting }) =
             <div className={styles.container}>
                 <div className={styles.town}>
                     {mainText}
-                    {arrow && <IoIosArrowDown className={styles.arrow}/>}
+                    {arrow && <IoIosArrowDown className={styles.arrow} />}
                 </div>
                 <div className={styles.menus}>
-                    {hamburger && <RxHamburgerMenu/>}
-                    {search && <IoMdSearch/>}
-                    {bell && <LuBell onClick={() => setOpenNotice(!openNotice)}/>}
-                    {setting && <IoSettingsOutline/>}
+                    {hamburger && <RxHamburgerMenu />}
+                    {search && <IoMdSearch />}
+                    {bell && <LuBell onClick={() => setOpenNotice(!openNotice)} />}
+                    {setting && <IoSettingsOutline />}
                 </div>
             </div>
-            {openNotice && <NoticeModal setOpenNotice={setOpenNotice}/>}
+            {openNotice && <NoticeModal setOpenNotice={setOpenNotice} />}
         </>
     );
 };

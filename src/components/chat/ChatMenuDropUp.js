11840ee5ee0de31e77d778cc69a8c07585385ac6
hgb926 <hgb926@naver.com>
2025-01-14 13:39:29 +0900

    [Feat] 대화 삭제 기능 완성

--- src/components/chat/ChatMenuDropUp.js
+++ src/components/chat/ChatMenuDropUp.js
@@ -1,28 +1,34 @@
 import React from 'react';
 import styles from '../../styles/components/ChatMenuDropUp.module.scss'
-import {useParams} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 import {CHAT_URL} from "../../config/host-config";
 
 
 const ChatMenuDropDown = ({ setMenuDrop }) => {
 
-    let {id: roomId} = useParams();
-    console.log(roomId)
+    const {id: roomId} = useParams();
+    const navi = useNavigate();
 
     const deleteHandler = async () => {
-        await fetch(`${CHAT_URL}/delete/${roomId}`, {
+
+        const response = await fetch(`${CHAT_URL}/delete/${roomId}`, {
             method: 'DELETE',
             credentials: 'include'
-        })
+        });
+        console.log(response)
+        if (response.status === 200) {
+            alert('대화가 종료되었습니다')
+            navi(-1)
+        }
     }
 
     return (
         <div className={styles.container}>
-            <ul className={styles.menuWrap}>
-                <li className={styles.menu}>신고하기</li>
-                <li className={styles.menu} onClick={() => setMenuDrop(false)}>닫기</li>
-                <li className={`${styles.menu} ${styles.exit}`} onClick={deleteHandler}>나가기</li>
-            </ul>
+            <div className={styles.menuWrap}>
+                <div className={styles.menu}>신고하기</div>
+                <div className={styles.menu} onClick={() => setMenuDrop(false)}>닫기</div>
+                <div className={`${styles.menu} ${styles.exit}`} onClick={deleteHandler}>나가기</div>
+            </div>
         </div>
     );
 };

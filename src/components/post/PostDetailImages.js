fd38dad77e0b4c70b8657345ba312c45d0a0a062
hgb926 <hgb926@naver.com>
2025-01-17 16:03:01 +0900

    [Feat] PostDetail 이미지 슬라이드

--- src/components/post/PostDetailImages.js
+++ src/components/post/PostDetailImages.js
@@ -3,24 +3,35 @@ import styles from "../../styles/pages/PostDetail.module.scss";
 import ControlArrow from "../ControlArrow";
 import Dots from "../Dots";
 
-const PostDetailImages = ({ post, checkClickTarget,currentIndex, changeImage }) => {
+const PostDetailImages = ({ post, checkClickTarget, currentIndex, changeImage }) => {
     return (
         <div>
-            <div className={styles.pic}
-                 onClick={(e) => checkClickTarget(e)}
-                 style={{
-                     backgroundImage: `url(${post.images[currentIndex]})`,
-                     backgroundSize: 'cover',
-                     backgroundPosition: 'center'
-                 }}>
-                {post.images.length > 1 &&
-                    <ControlArrow
-                        changeImg={changeImage}
-                    />}
-                <Dots
-                    length={post.images.length}
-                    currentIdx={currentIndex}
-                />
+            <div
+                className={styles.picWrapper}
+                onClick={(e) => checkClickTarget(e)}
+            >
+                <div
+                    className={styles.pic}
+                    style={{
+                        transform: `translateX(-${currentIndex * 100}%)`, // 슬라이드 이동
+                    }}
+                >
+                    {post.images.map((image, idx) => (
+                        <div
+                            key={idx}
+                            className={styles.slide}
+                            style={{
+                                backgroundImage: `url(${image})`,
+                                backgroundSize: 'cover',
+                                backgroundPosition: 'center',
+                            }}
+                        ></div>
+                    ))}
+                </div>
+                {post.images.length > 1 && (
+                    <ControlArrow changeImg={changeImage} />
+                )}
+                <Dots length={post.images.length} currentIdx={currentIndex} />
             </div>
         </div>
     );

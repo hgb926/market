f8825c03724d13285da76166c56e93ef71573c65
hgb926 <hgb926@naver.com>
2025-01-19 11:16:23 +0900

    [Feat] 서버에서 관심목록 데이터 받기

--- src/pages/LikedPosts.js
+++ src/pages/LikedPosts.js
@@ -1,12 +1,32 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import MyPageHeader from "../components/auth/MyPageHeader";
 import styles from '../styles/pages/Settings.module.scss';
 import Spinner from "../skeleton/Spinner";
+import {POST_URL} from "../config/host-config";
 
 const LikedPosts = () => {
 
     const userId = localStorage.getItem('id');
     const [loading, setLoading] = useState(true)
+    const [LikedList, setLikedList] = useState([])
+
+    const getLikedList = async () => {
+        setLoading(true)
+        let response = await fetch(`${POST_URL}/liked/${userId}`);
+        if (response.status === 200) {
+            let responseData = await response.json();
+            setLikedList(responseData)
+            console.log(responseData)
+            setLoading(false)
+        } else {
+            let responseData = await response.json();
+            console.log(responseData)
+        }
+    }
+
+    useEffect(() => {
+        getLikedList()
+    }, []);
 
 
     if (loading) return <Spinner />

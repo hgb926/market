c7b9102fe356b5413cfa47dbdbaf5ebc95356eea
hgb926 <hgb926@naver.com>
2025-01-19 20:07:35 +0900

    [Feat] 404에러 리다이렉트 페이지

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import {useRouteError} from "react-router-dom";
+import RootLayout from "../layout/RootLayout";
+import MainPage from "./MainPage";
 
 const ErrorPage = () => {
 
     const error = useRouteError();
 
-    return (
-        <div>
+    let errorMessage;
+    if (error.status === 404) {
+        errorMessage = "페이지를 찾을 수 없습니다. URL을 확인하세요."
+    }
 
-        </div>
+    return (
+        <>
+            <RootLayout/>
+            <div style={{
+                fontSize: "1.5rem",
+                textAlign: "center",
+                left: '50%',
+                top: '30%',
+                transform: 'translate(-50%, -50%)',
+                zIndex: 9999,
+                color: 'white',
+                position: 'absolute',
+                lineHeight: '2rem',
+            }}>
+                {errorMessage}
+            </div>
+        </>
     );
 };
 

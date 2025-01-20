18505d786a06547dab420037505b7b2226e3411f
hgb926 <hgb926@naver.com>
2025-01-20 15:04:33 +0900

    [Feat] 검색 인풋 입력 시 지우기 버튼 활성화

--- src/components/search/SearchHeader.js
+++ src/components/search/SearchHeader.js
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import styles from "../../styles/pages/PostSearchPage.module.scss";
 import {SlArrowLeft} from "react-icons/sl";
 import {useSelector} from "react-redux";
 import {sliceTownName} from "../../utils/sliceAddress";
 import {useNavigate} from "react-router-dom";
+import {LiaTimesSolid} from "react-icons/lia";
 
 const SearchHeader = () => {
 
+    const [keyword, setKeyword] = useState('')
+    const [showClearBtn, setShowClearBtn] = useState(false)
     let userData = useSelector(state => state.userInfo.userData);
     const townName = sliceTownName(userData.address)
     const navi = useNavigate();
+    const inputRef = useRef('');
 
     return (
         <div className={styles.searchContainer}>
             <SlArrowLeft className={styles.icon} onClick={() => navi(-1)}/>
-            <input className={styles.searchInput} placeholder={`${townName} 근처에서 검색`}/>
+            <input
+                className={styles.searchInput}
+                placeholder={`${townName} 근처에서 검색`}
+                ref={inputRef}
+                onChange={(e) => setKeyword(e.target.value)}
+            />
+            { keyword.length ?
+                <LiaTimesSolid
+                    className={styles.clearBtn}
+                    onClick={() => {
+                        inputRef.current.value = '';
+                        setKeyword('');
+                    }}
+                /> : ''
+            }
             <span className={styles.close} onClick={() => navi(-1)}>닫기</span>
         </div>
     );
